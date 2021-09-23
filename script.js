acbd0a0bcc547f15761c37ee7cd4fbacc89734e2
Cole Cochran <colecochran405@gmail.com>
2021-09-23 17:50:55 -0500

    transforming var to arrays.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numberChars = "0123456789";
-var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-var specialChars = "!,@,#,$,%,^,&,*,";
+//variables for characters for password generator. 
+var numberChar = [0,1,2,3,4,5,6,7,8,9];
+var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerChar = ['a','b','c','d','e','f','g','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specialChar = ['!','@','#','$','%','^','&','*',];
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -16,3 +19,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+var passwordLength = 0;
+
+//while (password < length || passwordLength > 128){
+//var passwordLength = prompt("Choose a number between 8 and 128 for your new password.");
+//}
